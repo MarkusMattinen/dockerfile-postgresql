ec9ace6e4ce772f6c6f05664db7c15c9188d0b4f
Markus Mattinen <markus.mattinen@vincit.fi>
2018-04-08 17:49:19 +0300

    Also chown log directory

--- 9.4/bin/entrypoint.sh
+++ 9.4/bin/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-chown -R postgres:postgres /var/lib/postgresql/*/main /etc/postgresql/*/main
+chown -R postgres:postgres /var/lib/postgresql/*/main /etc/postgresql/*/main /var/log/postgresql
 exec gosu postgres "$@"
